d46d61cfdb8ee525c74b30258c622851c6635727
Heather Runyon <hrocco25@gmail.COM>
2020-02-22 17:08:54 -0700

    gitignore

--- keys.py
+++ keys.py
@@ -1,2 +1,6 @@
 access= 'AKIAWUNESWIFOLDRFZWZ'
 secret= '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'
+
+[default]
+aws_access_key = 'AKIAWUNESWIFOLDRFZWZ'
+aws_secret_key = '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'
